7cde61cbc196d4dbb158146b1dfdf31739513bf7
wanghaijing <wanghaijing@edianzu.cn>
2018-09-28 21:23:08 +0800

    2

---
+++ config/config.py
@@ -0,0 +1,8 @@
+# encoding: utf-8
+import config_stage as config
+driver = config.driver
+MALL_HOST = config.MALL_HOST
+MALL_USER = config.MALL_USER
+MALL_PASSWD = config.MALL_PASSWD
+TOKEN = config.TOKEN
+WAIT_TIME = 3
---
+++ config/config_stage.py
@@ -0,0 +1,15 @@
+#encoding: utf-8
+import os
+from selenium import webdriver
+#driver = webdriver.Firefox()
+driver = webdriver.Chrome()
+
+#商城首页
+MALL_HOST = "http://www.edianzu.cn"
+MALL_USER = "18510155396"
+MALL_PASSWD = "edz123456"
+WAIT_TIME = 5
+# MALL_HOST = "http://stage.www.edianzu.cn"
+# MALL_USER = "15820181553"
+# MALL_PASSWD = "edz123456"
+# WAIT_TIME = 5
